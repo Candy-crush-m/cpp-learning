f31f4d54a3b308b117e77d9e8e2e0493c792e30d
Candy-crush-m <2444814953@qq.com>
2026-07-04 15:56:21 +0800

    新增 2_2_4_Name_Scope 练习代码
    新增2_2_4练习

---
+++ Chapter_2/2_2_4_Name_Scope/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //下面的程序合法吗？如果合法，它将输出什么？
+    int i = 0,sum = 0;
+    for (int i = 0;i != 10; ++i)
+    {
+        sum += i;
+    }
+    cout << i << " " << sum << endl;
+
+
+    system("pause");
+    return 0;
+}
+
+
+
+
+
+//
+// Created by zzm on 2026/7/4.
+//

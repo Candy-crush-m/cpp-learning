01929972369628ebe8092c0b00eb39ed0533db78
Candy-crush-m <2444814953@qq.com>
2026-07-04 14:33:59 +0800

    Add files via upload

---
+++ Chapter_2/2_1_3_Literal_Constant/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //请利用转义序列编写一段程序，要求先输出2M，然后转到新一行。
+    cout << "2M\n" ;
+    //修改程序使其先输出2，然后输出制表符，再输出M，最后转到新一行。
+    //双斜杠//表示一个斜杠！ /
+    cout << "2\tM\n";
+
+
+    system("pause");
+    return 0;
+}
+
+
+
+
+
+//
+// Created by zzm on 2026/7/4.
+//
